84dbe3d69b36a427c944683867cb6a6e6b3646e7
Tanja Hennis <tmhennis@gmail.com>
2017-02-22 14:42:19 +0100

    sliceable fruit

--- animations.js
+++ animations.js
@@ -6,27 +6,70 @@ function startAnimation() {
   let animation = document.createElement('div');
 
   animation.className = "animation";
-  animation.style['animation-duration'] = '4s';
+  // animation.style['animation-duration'] = '1s';
 
   container.appendChild(animation);
   document.querySelector('.mainDiv').appendChild(container);
 
-  // removing circles
   animation.addEventListener('animationend', function(event) {
     event.target.parentNode.parentNode.removeChild(event.target.parentNode);
-  })
+  });
 
-  animation.addEventListener('mouseover', function() {
-    console.log('mouseover success!')
-    animation.style.backgroundColor = 'red';
-  })
+  return animation;
 }
 
-// fire multiple objects onclick
-function loop() {
-  let rand = Math.round(Math.random() * 1000);
-  setTimeout(function() {
-    startAnimation();
-    loop();
-  }, rand);
+function initializeGame() {
+  let mouseDown = false;
+
+  document.addEventListener('mousedown', function() {
+    mouseDown = true;
+  });
+
+  document.addEventListener('mouseup', function() {
+    mouseDown = false;
+  });
+
+  (function loop() {
+      let rand = Math.round(Math.random() * 3000);
+      setTimeout(function() {
+        trackAnimation(startAnimation());
+        loop();
+      }, rand);
+    }
+  )();
+
+  function trackAnimation(animation) {
+    animation.addEventListener('mouseleave', function(event) {
+      if(mouseDown) {
+        const { left, top } = animation.getBoundingClientRect();
+
+        event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+
+        let halfFruitDown = document.createElement('div');
+        halfFruitDown.className = 'halfFruitDown';
+
+        halfFruitDown.style.left = left + "px";
+        halfFruitDown.style.top = top + "px";
+
+        halfFruitDown.addEventListener('animationend', function(event) {
+          event.target.parentNode.removeChild(event.target);
+        });
+
+        document.querySelector('.mainDiv').appendChild(halfFruitDown);
+
+        let halfFruitUp = document.createElement('div');
+        halfFruitUp.className = 'halfFruitUp';
+
+        halfFruitUp.style.left = left + "px";
+        halfFruitUp.style.top = top + "px";
+
+        halfFruitUp.addEventListener('animationend', function(event) {
+          event.target.parentNode.removeChild(event.target);
+        });
+
+        document.querySelector('.mainDiv').appendChild(halfFruitUp);
+
+      }
+    });
+  }
 }
